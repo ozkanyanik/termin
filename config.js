1243a4b9716404f42b0a5c05ec0b5ca269ba7c54
Ozkan YANIK <ozkanyanik@hotmail.com>
2025-12-17 19:20:23 +0100

    Update configuration for TOKEN and SMTP settings

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 {
-  "TOKEN": "Bearer BurayaToken",
+  "TOKEN": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE3NjU5ODkwMjMsImp0aSI6IjYzMDIzMzU4NyIsImlzcyI6Imh0dHBzOlwvXC9zdHV0dGdhcnQua29uc2VudGFzLmRlXC8iLCJleHAiOjE3NjYwMTc4MjMsImRhdGEiOnsic2lnbnVwZm9ybV9pZCI6Mywic2lnbnVwX2lkIjo2OTM4MjEwLCJ1c2VyX2lkIjpudWxsLCJvdGFfdXNlciI6Imd1ZXN0XzY5NDJlOGFmZjNhNzcifX0.rtV4e2oJFOu1xL06lyX8jVx5LhdA6l0W-U3VEDfG2mPFWtmlT-M7cOl1k0Oh98OR-1YeKpIVwswE1Wi43bkIJA",
   "EMAIL": {
-    "SMTP_HOST": "smtp.example.com",
+    "SMTP_HOST": "smtp.gmail.com",
     "SMTP_PORT": 587,
     "USER": "email@example.com",
     "PASS": "emailPassword",
-    "TO": "hedef@example.com"
+    "TO": "ozkanyanik@hotmail.com"
   }
 }
