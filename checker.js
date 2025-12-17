4495de86c42777c94789ff00b85c342cab933de1
Ozkan YANIK <ozkanyanik@hotmail.com>
2025-12-17 21:54:43 +0100

    Fix JSON serialization in response logging

--- checker.js
+++ checker.js
@@ -18,7 +18,7 @@ async function getDynamicToken() {
   });
 
   const json = await res.json();
-  console.log('Response: ' + JSON.serialize(json));
+  console.log('Response: ' + JSON.stringify(json));
   if (!json?.data?.ota_jwt) {
     throw new Error("❌ ota_jwt alınamadı");
   }
