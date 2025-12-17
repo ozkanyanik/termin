004b3e369b98b56bce759680e2f73dd3f922dabe
Ozkan YANIK <ozkanyanik@hotmail.com>
2025-12-17 19:17:55 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+{
+  "TOKEN": "Bearer BurayaToken",
+  "EMAIL": {
+    "SMTP_HOST": "smtp.example.com",
+    "SMTP_PORT": 587,
+    "USER": "email@example.com",
+    "PASS": "emailPassword",
+    "TO": "hedef@example.com"
+  }
+}
